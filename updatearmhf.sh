68049295b62baa76c15a8a6745eb4b45ef732f71
Christopher Homberger <christopher.homberger@web.de>
2019-12-21 23:26:38 +0100

    Fix again

--- updatearmhf.sh
+++ updatearmhf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-EXP=$(dpkg -l | grep :armhf | sed -E 's/ii +//g' | sed -E 's/:.*//g' | sed -E 's/.*-dev//g' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/:armhf /g' | sed -E 's/ +/ /g' | sed -E 's/ +$//g')
+EXP=$(dpkg -l | grep :armhf | sed -E 's/ii +//g' | sed -E 's/:.*//g' | sed -E 's/.*-dev//g' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' | sed -E 's/ +/ /g' | sed -E 's/ /:armhf /g' | sed -E 's/ +$//g')
 sed -i "s/DEPS_PLACEHOLDER/${EXP}/g" config-bionic-armhf.json
