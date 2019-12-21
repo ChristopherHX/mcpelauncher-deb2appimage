15c0494cbfa2b36fb7d749f19a983d845c3566bb
Christopher Homberger <christopher.homberger@web.de>
2019-12-21 22:06:03 +0100

    Next level

--- updatearmhf.sh
+++ updatearmhf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-EXP=$(dpkg -l | grep :armhf | sed -E 's/ii  ([a-z0-9\.\-]+).*/\1/g' | sed -e 's/.*-dev//g'  | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' | sed -E 's/ +/ /g' | sed -E 's/ +$//g')
+EXP=$(dpkg -l | grep :armhf | sed -E 's/ii +//g' | sed -E 's/:.*//g' | sed -E 's/.*-dev//g' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' | sed -E 's/ +/ /g' | sed -E 's/ +$//g')
 sed -i "s/DEPS_PLACEHOLDER/${EXP}/g" config-bionic-armhf.json
